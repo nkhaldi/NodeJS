dd9edf0ee0d7d4ee4a5f52ce9f11de36a054b5dd
Narek <mnk80898@gmail.com>
2023-07-09 21:05:39 +0400

    make my first programm on node.js

---
+++ hello.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name?\n", (name) => {
+  console.log(`Hello, ${name}!`);
+  rl.close();
+});
